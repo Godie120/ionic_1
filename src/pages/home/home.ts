8ea952bbd553564b7ec7752f9dd64f3d275feea1
Godie <godie120@hotmail.com>
2018-04-26 13:26:09 -0500

    Primer app ionic

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,13 +2,32 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+	selector: 'page-home',
+	templateUrl: 'home.html'
 })
 export class HomePage {
+	numLike: number = 0;
+	numComment: number = 0;
+	newComment: boolean = true;
+	comments = [];
+	btnColor: string = '';
 
-  constructor(public navCtrl: NavController) {
+	constructor(public navCtrl: NavController) {}
 
-  }
+	addLike() {
+		this.numLike++;
+		this.btnColor = 'red';
+	}
 
+	viewNewCommen() {
+		this.newComment = false;
+	}
+
+	addComment(newComment: string) {
+		this.numComment++;
+		let otrocomment = 'Usuario' + this.numComment + ': ' + newComment;
+
+		this.comments.push(otrocomment);
+		this.newComment = true;
+	}
 }
